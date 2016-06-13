3122bacc5ee4a601902482dc621fa63252f2ec90
Bandi <banve02@ca.com>
2016-06-13 17:00:24 +0530

    updating the repo

---
+++ src/PalindromeIndex.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PalindromeIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		String[] input = new String[size];
+		for(int i=0; i<size;i++){
+			in = new Scanner(System.in);
+			String s = in.next();	
+			input[i] = s;
+//			System.out.println(returnWrongIndex(s));
+		}
+		
+		for(int i=0; i<size;i++){
+			System.out.println(returnWrongIndex(input[i]));
+		}
+		
+	}
+
+	private static int returnWrongIndex(String s) {
+		// TODO Auto-generated method stub
+		int startIndex=0, endIndex=s.length()-1, wrongIndex=-1;
+		
+		while(startIndex<=endIndex){
+			if(s.charAt(startIndex)==s.charAt(endIndex)){
+				startIndex++;
+				endIndex--;
+			}
+			else if(s.charAt(startIndex+1)==s.charAt(endIndex)){
+				wrongIndex = startIndex;
+				break;
+			}
+			else if(s.charAt(startIndex)==s.charAt(endIndex-1)){
+				wrongIndex = endIndex;
+				break;
+			}
+		}
+		return wrongIndex;
+	}
+
+}
---
+++ src/Stock.java
@@ -0,0 +1,50 @@
+
+public class Stock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ar = {10, 22, 5, 75, 65, 80};
+		int maxTransaction=3;
+		System.out.println(maxProfit(ar, ar.length,maxTransaction)); 
+	}
+
+	static int maxProfit(int price[], int n, int k)
+	{
+	    // table to store results of subproblems
+	    // profit[t][i] stores maximum profit using
+	    // atmost t transactions up to day i (including
+	    // day i)
+	    int profit[][] = new int[k+1][n+1];
+	 
+	    // For day 0, you can't earn money
+	    // irrespective of how many times you trade
+	    for (int i = 0; i <= k; i++)
+	        profit[i][0] = 0;
+	 
+	    // profit is 0 if we don't do any transation
+	    // (i.e. k =0)
+	    for (int j= 0; j <= n; j++)
+	        profit[0][j] = 0;
+	 
+	    // fill the table in bottom-up fashion
+	    for (int i = 1; i <= k; i++)
+	    {
+	        for (int j = 1; j < n; j++)
+	        {
+	            int max_so_far = -1;
+	 
+	            for (int m = 0; m < j; m++)
+	                max_so_far = Math.max(max_so_far,
+	                   price[j] - price[m] + profit[i-1][m]);
+	 
+	            profit[i][j] = Math.max(profit[i][j-1], max_so_far);
+	            System.out.println(profit[i][j]);
+	            
+	        }
+	        System.out.println("++++++++++");
+	    }
+	 
+	    return profit[k][n-1];
+	}
+
+}
