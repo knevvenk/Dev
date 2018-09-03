5305bdb9d48ec4013169bd95faf0a344d2ef9b5a
banve02 <banve02@ca.com>
2018-09-03 14:57:40 +0530

    Print odd even

--- src/LamdaEx.java
+++ src/LamdaEx.java
@@ -1,9 +1,15 @@
+import com.sun.nio.file.ExtendedCopyOption;
+
 /**
  * Created by bandi on 1/7/2016.
  */
 
 interface Executable{
     void execute(String s);    
+    
+    default void test() {
+    	System.out.println("Default Method");
+    }
 }
 
 class Runner{
@@ -11,6 +17,7 @@ class Runner{
         System.out.println("In run method");
         e.execute("venkat");
     }
+
 }
 public class LamdaEx {
     public static void main(String[] args) {
@@ -24,7 +31,12 @@ public class LamdaEx {
         });
 
         System.out.println("================================");
-        runner.run((String s1) -> System.out.println("In lamdaEx execute method "+s1+" "+i));
+        runner.run(s1 -> System.out.println("In lamdaEx execute method "+s1+" "+i));
+        
+        System.out.println("********************");
+        Executable e = s1 -> System.out.println("In lamdaEx execute method "+s1+" "+i);
+        e.execute("Bandi");
+        e.test();
 //        i =9;
     }
 }
